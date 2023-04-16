f497aedd5098f8389b6dc0dd73f29aa9c84da023
Adam Nasr <aboodnasr199@gmail.com>
2023-04-16 15:28:33 -0500

    committing biography

--- src/Author.java
+++ src/Author.java
@@ -1,4 +1,16 @@
-public class Author {
+import java.util.ArrayList;
+
+ public class Author {
+
+
+    public Author(String firstName, String lastName, String country, boolean isAlive, int age, ArrayList<Book> listOfBooks) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.country = country;
+        this.isAlive = isAlive;
+        this.age = age;
+        this.listOfBooks = listOfBooks;
+    }
 
     /**
      * WORK ON BOOK CLASS FIRST
@@ -6,20 +18,30 @@ public class Author {
      Define Author class fields as firstName, lastName, country, isAlive, age, list of books
      */
 
-    //Create 6 args custom constructor here
-    //YOUR CODE HERE
-
-
     /*
         Define instance variables here
         NOTE: Books must be defined as a List of Objects of Book class not String
     */
-    //YOUR CODE HERE
+    public String firstName;
+    public String lastName;
+    public String country;
+    public boolean isAlive;
+    public int age;
+    public ArrayList<Book> listOfBooks;
 
 
     /*
     Override toString() method here that returns Author object information
      */
-    //YOUR CODE HERE
 
+    @Override
+    public String toString() {
+        return "Author{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", country='" + country + '\'' +
+                ", isAlive=" + isAlive +
+                ", age=" + age + "}";
+    }
 }
+
--- src/Biography.java
+++ src/Biography.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Biography {
     public static void main(String[] args) {
 
@@ -30,8 +33,61 @@ public class Biography {
         Book{name='The Royal Game', tale='novella', page=53}
         Book{name='24 Hours in the Life of a Woman', tale='novella', page=80}
          */
+        Scanner input = new Scanner(System.in);
 
-        //YOUR CODE HERE
+        ArrayList<Book> listOfBooks = new ArrayList<>();
+
+
+        System.out.println("What is your favorite author's first name?");
+        String fName = input.nextLine();
+
+        System.out.println("What is your favorite author's last name?");
+        String lName = input.nextLine();
+
+        System.out.println("Where is your favorite author from?");
+        String country = input.nextLine();
+
+        System.out.println("Is your favorite author alive? Y/N");
+        boolean isAlive = input.nextLine().equalsIgnoreCase("Y");
+        int age= 0;
+        if (isAlive){
+            System.out.println("How old is your favorite author?");
+            age = input.nextInt();
+            input.nextLine();
+        }
+        Author author = new Author(fName, lName, country, isAlive, age, listOfBooks);
+
+        System.out.println("Would you like to enter book information? Y/N");
+        String addBook = input.nextLine();
+        while (addBook.equalsIgnoreCase("Y")){
+            System.out.println("What is the book name?");
+            String title = input.nextLine();
+
+            System.out.println("What is the genre of the book?");
+            String genre = input.nextLine();
+
+            System.out.println("How many pages does the book have?");
+            int pages = input.nextInt();
+            input.nextLine();
+
+            Book book = new Book(title, genre, pages);
+            listOfBooks.add(book);
+
+            System.out.println("Would you like to enter another book? Y/N");
+            addBook = input.nextLine();
+
+        }
+        System.out.println("Author's information = " + author.toString());
+
+        System.out.println("Author's books as listed below:");
+        for (Book element : listOfBooks) {
+            System.out.println(element);
+
+        }
+
+
+
+
 
     }
 }
